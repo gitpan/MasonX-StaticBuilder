6a12421766b0c4a6fdcb67fab92d9b765eb4013f
Alex Skud Bayley <skud@infotrope.net>
2007-03-29 00:43:30 +0000

    Import of SKUD/MasonX-StaticBuilder-0.04 from CPAN.
    gitpan-cpan-distribution: MasonX-StaticBuilder
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SKUD/MasonX-StaticBuilder-0.04.tar.gz
    gitpan-cpan-author:       SKUD
    gitpan-cpan-maturity:     released

--- lib/MasonX/StaticBuilder.pm
+++ lib/MasonX/StaticBuilder.pm
@@ -3,7 +3,7 @@ package MasonX::StaticBuilder;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use base qw(Class::Accessor);
 MasonX::StaticBuilder->mk_accessors(qw(input_dir));
